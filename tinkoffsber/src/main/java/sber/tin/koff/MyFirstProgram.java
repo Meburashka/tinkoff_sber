e19c9b62031b2cab6fa5cc53037c8cdd44703755
Meburashka <katuha_21@mail.ru>
2017-10-30 19:20:56 +0300

    Тест

---
+++ tinkoffsber/src/main/java/sber/tin/koff/MyFirstProgram.java
@@ -0,0 +1,12 @@
+package sber.tin.koff;
+
+public class MyFirstProgram {
+
+	public static void main(String[] args) {
+		hello();
+	}
+
+	public static void hello() {
+		System.out.println("Hello, world!");
+	}
+}
